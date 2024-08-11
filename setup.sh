ffbb0056549f0d2fb2966110fb857d6f5816a658
forrestbthomas <forrestbthomas@gmail.com>
2024-08-11 09:57:39 -0700

    fetch and use zshrc

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
+# assume zsh not installed
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# add zsh profile
+curl -Lo ~/.zshrc https://raw.githubusercontent.com/forrestbthomas/config/main/.zshrc
+source ~/.zshrc
