51aae0f54a889c122a49c7d4fec7baf186bf4e8e
forrestbthomas <forrestbthomas@gmail.com>
2024-08-11 10:39:43 -0700

    add fleek

--- .zshrc
+++ .zshrc
@@ -108,3 +108,9 @@ func src-it() {
 source ~/.zshrc
 }
 
+func zp() {
+vim ~/.zshrc
+
+### STARTUP ###
+eval "$(devbox global shellenv)"
+}
--- setup.sh
+++ setup.sh
@@ -6,3 +6,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # add zsh profile
 curl -Lo ~/.zshrc https://raw.githubusercontent.com/forrestbthomas/config/main/.zshrc
 source ~/.zshrc
+
+# install devbox (will also install NixOS)
+curl -fsSL https://get.jetify.com/devbox | bash # not sure why it needs to be bash
+
+# install Fleek "high" profile
+devbox global pull https://devbox.getfleek.dev/high
